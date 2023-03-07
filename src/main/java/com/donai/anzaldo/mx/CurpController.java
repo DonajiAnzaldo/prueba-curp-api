711a76f088b5851bec766ae354df47312e7cdb49
Donaji Anzaldo <rdaolmos@gmail.com>
2023-03-07 09:28:31 -0700

    curp api

--- src/main/java/com/donai/anzaldo/mx/CurpController.java
+++ src/main/java/com/donai/anzaldo/mx/CurpController.java
@@ -2,6 +2,13 @@ package com.donai.anzaldo.mx;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//importaciones de la API
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Controller
 public class CurpController {
@@ -9,5 +16,10 @@ public class CurpController {
 	@GetMapping("/curp")
 	public String consultarCurp() {
 		return "index";
+	}
+
+	@GetMapping("/curpAPI")
+	public String consultarAPI() {
+		return "curpAPI";
 	}
 }
